ca12cc3f5fbd441465fd6a7267c4ef05925b2f55
adamzulkarnain <adam.zulkarnain@paper.id>
2021-02-08 00:21:20 +0700

    create service to get data from omdb

---
+++ services/omdb_service.go
@@ -0,0 +1,44 @@
+package services
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"omdbapi/objects"
+	"os"
+)
+
+type OmdbService struct {
+}
+
+func OmdbServiceHandler() OmdbService {
+	return OmdbService{}
+}
+
+func (o OmdbService) GetList(params objects.OmdbAPIListRequestObject) (objects.OmdbAPIListResponseObject, error) {
+
+	if params.Page == 0 {
+		params.Page = 1
+	}
+
+	url := fmt.Sprintf("http://www.omdbapi.com/?apikey=%s&s=%s&page=%d", os.Getenv("OMDB_API_KEY"), params.Search, params.Page)
+	requestToOmdb, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return objects.OmdbAPIListResponseObject{}, err
+	}
+	httpClient := &http.Client{}
+	response, err := httpClient.Do(requestToOmdb)
+	if err != nil {
+		return objects.OmdbAPIListResponseObject{}, err
+	}
+	fmt.Printf("%v \n", response.Body)
+	if int(response.StatusCode/100) != 2 {
+		return objects.OmdbAPIListResponseObject{}, fmt.Errorf("status %d", response.StatusCode)
+	}
+	var omdbAPIResponse objects.OmdbAPIListResponseObject
+	err = json.NewDecoder(response.Body).Decode(&omdbAPIResponse)
+	if err != nil {
+		return objects.OmdbAPIListResponseObject{}, err
+	}
+	return omdbAPIResponse, nil
+}
